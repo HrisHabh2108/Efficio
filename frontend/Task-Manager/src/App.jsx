9457bfd1b1cf38e407ee2f9b80bc6180a6f72eba
HrisHabh2108 <hrishabh.deshmukh.ece22@itbhu.ac.in>
2025-08-11 16:14:54 +0530

    First commit

---
+++ frontend/Task-Manager/src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useContext } from 'react'
+import{BrowserRouter as Router,Routes,Route, Navigate, Outlet}from "react-router-dom";
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import PrivateRoute from './routes/PrivateRoute';
+import Dashboard from "./pages/Admin/Dashboard";
+import ManageTask from "./pages/Admin/ManageTask";
+import CreateTask from "./pages/Admin/CreateTask";
+import ManageUsers from "./pages/Admin/ManageUsers";
+import UserDashboard from './pages/User/UserDashboard';
+import MyTask from './pages/User/MyTask';
+import ViewTaskDetails from './pages/User/ViewTaskDetails';
+import UserProvider, { UserContext } from './context/userContext';
+
+
+const App = () => {
+  return (
+    <UserProvider>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<SignUp/>}/> 
+
+          <Route element={<PrivateRoute allowedRoles={["admin"]}/>}>
+            <Route path="/admin/dashboard" element={<Dashboard/>}/>
+            <Route path="/admin/tasks" element={<ManageTask/>}/>
+            <Route path="/admin/create-task" element={<CreateTask/>}/>
+            <Route path="/admin/users" element={<ManageUsers/>}/>
+          </Route>      
+
+          <Route element={<PrivateRoute allowedRoles={["admin"]}/>}>
+            <Route path="/user/dashboard" element={<UserDashboard/>}/>
+            <Route path="/user/tasks" element={<MyTask/>}/>
+            <Route path="/user/task-details/:id" element={<ViewTaskDetails/>}/>
+          </Route>
+
+          {/*Default Route*/}
+          <Route path="/" element={<Root/>}/>
+
+        </Routes>
+      </Router>
+    </div>
+    </UserProvider>
+  )
+}
+
+export default App;
+
+const Root=()=>{
+    const {user,loading}=useContext(UserContext);
+
+    if(loading)return<Outlet/>
+
+    if(!user){
+      return <Navigate to="/login" />;
+    }
+
+    return user.role==="admin"?<Navigate to="/admin/dashboard"/>:<Navigate to="/user/dashboard"/>
+}
---
+++ frontend/Task-Manager/src/pages/User/MyTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyTask = () => {
+  return (
+    <div>
+      User-task
+    </div>
+  )
+}
+
+export default MyTask
