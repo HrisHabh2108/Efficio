543bb5c329219250ccd69c39ddd16d64e9c04592
HrisHabh2108 <hrishabh.deshmukh.ece22@itbhu.ac.in>
2025-11-23 02:58:33 +0530

    fixed the Admin still bug with the download report

--- frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
+++ frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
 import { PRIORITY_DATA } from '../../utils/data'
 import axiosInstance from '../../utils/axiosInstance';
@@ -11,6 +11,9 @@ import SelectDropdown from '../../components/Inputs/SelectDropdown';
 import SelectUsers from '../../components/Inputs/SelectUsers';
 import TodoListInput from '../../components/Inputs/TodoListInput';
 import AddAttachmentsInput from '../../components/Inputs/AddAttachmentsInput';
+import Modal from '../../components/layouts/Modal';
+import DeleteAlert from '../../components/layouts/DeleteAlert';
+
 const CreateTask = () => {
   const location = useLocation();
   const { taskId } = location.state || {};
@@ -74,7 +77,37 @@ const CreateTask = () => {
     }
   };
 
-  const updateTask = async () => {};
+  const updateTask = async () => {
+    setLoading(true);
+    try{
+      const todoList = taskData.todoChecklist?.map((item) => {
+        const prevTodoChecklist = currentTask?.todoChecklist || [];
+        const matchedTask = prevTodoChecklist.find((task) => task.text == item);
+
+      return {
+        text: item,
+        completed: matchedTask ? matchedTask.completed : false,
+      };
+    });
+
+      const response = await axiosInstance.put(
+        API_PATHS.TASKS.UPDATE_TASK(taskId),
+        {
+          ...taskData,
+          dueDate: new Date(taskData.dueDate).toISOString(),
+          todoChecklist: todoList,
+        }
+      );
+
+      toast.success("Task Updated Successfully");
+    }catch(error){
+      console.error("Error creating task:",error);
+      setLoading(false);
+    }finally{
+      setLoading(false);
+    }
+  };
+
   const handleSubmit = async () => {
     setError(null);
 
@@ -107,8 +140,54 @@ const CreateTask = () => {
     createTask();
   };
 
-  const getTaskDetailsByID = async () => {};
-  const deleteTask = async () => {};
+  const getTaskDetailsByID = async () => {
+  try {
+    const response = await axiosInstance.get(
+      API_PATHS.TASKS.GET_TASK_BY_ID(taskId)
+    );
+
+    if (response.data) {
+      const taskInfo = response.data;
+      setCurrentTask(taskInfo);
+
+      setTaskData((prevState) => ({
+        title: taskInfo.title,
+        description: taskInfo.description,
+        priority: taskInfo.priority,
+        dueDate: taskInfo.dueDate
+          ? moment(taskInfo.dueDate).format("YYYY-MM-DD")
+          : null,
+        assignedTo: taskInfo?.assignedTo?.map((item) => item?._id) || [],
+        todoChecklist: taskInfo?.todoChecklist?.map((item) => item?.text) || [],
+        attachments: taskInfo?.attachments || [],
+      }));
+    }
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+};
+  // Delete Task
+  const deleteTask = async () => {
+    try {
+      await axiosInstance.delete(API_PATHS.TASKS.DELETE_TASK(taskId));
+      setOpenDeleteAlert(false);
+      toast.success("Expense details deleted successfully");
+      navigate('/admin/tasks');
+    } catch (error) {
+      console.error(
+        "Error deleting expense:",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
+  useEffect(()=>{
+    if(taskId){
+      getTaskDetailsByID(taskId)
+    }
+    return ()=>{
+    }
+  },[taskId]);
 
    return (
     <DashboardLayout activeMenu="Create Task">
@@ -228,6 +307,17 @@ const CreateTask = () => {
           </div>
         </div>
       </div>
+
+      <Modal
+        isOpen={openDeleteAlert}
+        onClose={()=>setOpenDeleteAlert(false)}
+        title="Delete Task"
+      >
+        <DeleteAlert 
+          content="Are you sure you want to delete this task?"
+          onDelete={()=>deleteTask()}
+        />
+      </Modal>
     </DashboardLayout>
   );
 }
